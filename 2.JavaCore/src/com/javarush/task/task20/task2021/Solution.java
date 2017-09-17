9c613cc3ed6fbab7ba05e6c9f8b724496c93de22
script972 <script972@gmail.com>
2017-09-17 21:06:02 +0300

    start bit ex "restoran"

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        public void writeObject(ObjectOutputStream ob) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+
+        public void readObject(ObjectInputStream ob) throws NotSerializableException {
+            throw new NotSerializableException();
+
+        }
+    }
+
+    public static void main(String[] args) throws NotSerializableException {
+
+    }
+}
