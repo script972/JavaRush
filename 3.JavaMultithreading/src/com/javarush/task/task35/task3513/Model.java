1ad4027ca9c89d8b6765b5bd62ef592e7e1ae6ce
script972 <script972@gmail.com>
2017-09-22 23:34:07 +0300

    2048 3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task35.task3513;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    private static final int FIELD_WIDTH = 4;
+
+    private Tile [][] gameTiles;
+
+    public Model() {
+        gameTiles=new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[0].length; j++) {
+                gameTiles[i][j]=new Tile();
+            }
+        }
+        resetGameTiles();
+    }
+
+    // вовзращает лист пустых клеток
+    private List<Tile> getEmptyTiles() {
+        List<Tile> result = new ArrayList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (gameTiles[i][j].value == 0) result.add(gameTiles[i][j]);
+            }
+        }
+        return result;
+    }
+
+    // добавляет рандомно клетку 2 или 4 (соотношение 1 к 9)
+    private void addTile() {
+
+        List<Tile> list = getEmptyTiles();
+        if (list != null && list.size() != 0) {
+            list.get((int) (list.size() * Math.random())).setValue(Math.random() < 0.9 ? 2 : 4);
+        }
+    }
+
+    // сброс всех клеток
+    void resetGameTiles() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                this.gameTiles[i][j] = new Tile();
+            }
+        }
+        addTile();
+        addTile();
+    }
+}
