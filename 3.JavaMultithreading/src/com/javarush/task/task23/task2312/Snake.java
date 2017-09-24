71cf5965a7a9074598b034cfcecb7282e2e28fe3
script972 <script972@gmail.com>
2017-09-24 21:12:31 +0300

    tetris

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -44,42 +44,34 @@ public class Snake {
     }
 
     public void move(){
-        if(isAlive){
-            switch (direction){
-                case UP: move(0, -1); break;
-                case RIGHT: move(1, 0); break;
-                case DOWN: move(0, 1); break;
-                case LEFT: move(-1, 0); break;
-            }
-        }
+        if (!isAlive) return;
+        if (direction == SnakeDirection.UP)
+            move(0, -1);
+        else if (direction == SnakeDirection.RIGHT)
+            move(1, 0);
+        else if (direction == SnakeDirection.DOWN)
+            move(0, 1);
+        else if (direction == SnakeDirection.LEFT)
+            move(-1, 0);
     }
-
 
     public void move(int dx, int dy){
         SnakeSection head = sections.get(0);
         head = new SnakeSection(head.getX() + dx, head.getY() + dy);
-
-        //Проверяем - не вылезла ли голова за границу комнаты
         checkBorders(head);
         if (!isAlive) return;
-
-        //Проверяем - не пересекает ли змея  саму себя
         checkBody(head);
         if (!isAlive) return;
-
-        //Проверяем - не съела ли змея мышь.
         Mouse mouse = Room.game.getMouse();
         if (head.getX() == mouse.getX() && head.getY() == mouse.getY()) //съела
         {
-            sections.add(0, head);                  //Добавили новую голову
-            Room.game.eatMouse();                   //Хвот не удаляем, но создаем новую мышь.
-        } else //просто движется
+            sections.add(0, head);
+            Room.game.eatMouse();
+        } else
         {
-            sections.add(0, head);                  //добавили новую голову
-            sections.remove(sections.size() - 1);   //удалили последний элемент с хвоста
+            sections.add(0, head);
+            sections.remove(sections.size() - 1);
         }
-
-
     }
 
     public void checkBorders (SnakeSection head){
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+
+    private Field field;
+    private Figure figure;
+
+    public static Tetris game;
+
+    public static void main(String[] args) {
+        game=new Tetris();
+        game.run();
+    }
+
+
+    public void run(){
+
+    }
+
+    public void step(){
+
+    }
+
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+}
