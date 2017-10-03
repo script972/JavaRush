5c5e4633b849b485501d2d5be38cf7138eced8d1
script972 <script972@gmail.com>
2017-10-04 00:58:03 +0300

    Custom SET

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,99 +1,87 @@
 package com.javarush.task.task37.task3707;
 
-import java.io.Closeable;
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
-public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Set<E>, Serializable {
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
     private static final Object PRESENT = new Object();
     private transient HashMap<E, Object> map;
-
     public AmigoSet() {
         map = new HashMap<>();
     }
-
-    public AmigoSet(int capacity) {
-        map = new HashMap<>(capacity);
+    public AmigoSet(Collection<? extends E> collection) {
+        map = new HashMap<>(Math.max(16, (int)(collection.size()/0.75f +1)));
+        this.addAll(collection);
     }
-
-
-    public AmigoSet(Collection c) {
-        map = new HashMap<>(Math.max((int) (c.size() / .75f) + 1, 16));
-        addAll(c);
-    }
-
-
-
     @Override
-    public Spliterator spliterator() {
-        return null;
-    }
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
 
+    }
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
-
-    @Override
-    public boolean add(E e) {
-        return map.put(e, PRESENT) == null;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        map.putAll((Map<? extends E, ?>) c);
-        return true;
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection c) {
-        return false;
+        return super.contains(o);
     }
 
     @Override
     public void clear() {
+        map.clear();
+    }
 
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try {
+            AmigoSet copy = (AmigoSet)super.clone();
+            copy.map = (HashMap) map.clone();
+            return copy;
+        }
+        catch (Exception e) {
+            throw new InternalError();
+        }
     }
 
 
+
+   // Спасибо. Действительно валидатор требует поэлементную запись ключей и запись размера через writeInt
+   //         (через writeObject почему-то не принимает, хотя результат сериализации от этого не зависит)
+
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        for (int i = 0; i < map.size(); i++) {
+            out.writeObject(map.get(i));
+        }
+        out.writeFloat(HashMapReflectionHelper.callHiddenMethod(map, "loadFactor"));
+        out.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+    }
+
+    //В десериализации ключи в AmigoSet не добавляю, заполняю ими map.
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        //defaultReadObject();
+        in.defaultReadObject();
+
+       /* * in.defaultReadObject();
+        Set keys = (Set)in.readObject();
+        float loadFactor = in.readFloat();
+        int capacity = in.readInt();
+        map = new HashMap(capacity, loadFactor);
+        addAll(keys);
+        * */
+    }
 }
