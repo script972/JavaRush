ed0ee6cf6d0b8a4f09e63e17e19dbf71e937fd05
script972 <script972@gmail.com>
2017-10-07 20:07:13 +0300

    aggregator 5

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -4,6 +4,8 @@ import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.model.Strategy;
 
 public class Aggregator  {
+//    https://github.com/mrShahid/JavaRushTasks/blob/master/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+
     public static void main(String[] args) {
         Provider provider=new Provider(new Strategy() {
         });
